ea5a0babccd60d3e5adf32baf7d691098f73c89b
Santiago Sala <pipo0716@gmail.com>
2024-08-27 10:58:54 +0300

    15- Zooming

--- ProjectPuzzle/Assets/Scripts/CameraController.cs
+++ ProjectPuzzle/Assets/Scripts/CameraController.cs
@@ -10,6 +10,14 @@ public class CameraController : MonoBehaviour
     private Vector3 _cameraStartMovePosition;
     private Vector2 _firstTouchScreenPosition;
 
+    [Header("Zoom")]
+    [SerializeField]private float _zoomMultiplier;
+    [SerializeField]private Vector2 _minMaximumOrthographicSize;
+    [SerializeField] private float _zoomSpeed;
+    private Vector3 _zoomInitialPosition;
+    private Vector3 _zoomCenter;
+    private float _currenOrthographicSize;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,5 +42,42 @@ public class CameraController : MonoBehaviour
         Vector3 targetPostiion = _cameraStartMovePosition - moveDelta * _moveSpeed;
         transform.position = targetPostiion;
     }
-    
+
+    public void DoubleTouchBeganCallback(Vector2 firstTouchPosition, Vector2 secondTouchPosition)
+    {
+        _currenOrthographicSize = Camera.main.orthographicSize;
+
+        _firstTouchScreenPosition = firstTouchPosition;
+
+        _zoomInitialPosition = transform.position;
+
+        _zoomCenter = ((Camera.main.ScreenToWorldPoint(firstTouchPosition) + Camera.main.ScreenToWorldPoint(secondTouchPosition)) / 2);
+        _zoomCenter.z = -10;
+    }
+
+    public void DoubleTouchDrag(float deltaMagnitude)
+    {
+        SetOrtographicSize(deltaMagnitude);
+
+        MoveTowardsZoomCenter();
+    }
+    private void SetOrtographicSize(float deltaMagnitude)
+    {
+        float targetOrthographicSize = _currenOrthographicSize - deltaMagnitude * _zoomMultiplier;
+        targetOrthographicSize = Mathf.Clamp(targetOrthographicSize, _minMaximumOrthographicSize.x, _minMaximumOrthographicSize.y);
+
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetOrthographicSize, Time.deltaTime * 60 * 0.3f);
+    }
+    private void MoveTowardsZoomCenter()
+    {
+        float percent = Mathf.InverseLerp(_minMaximumOrthographicSize.x, _minMaximumOrthographicSize.y, _currenOrthographicSize - Camera.main.orthographicSize);
+
+        percent *= _zoomSpeed;
+
+        Vector3 targetPosition = Vector3.Lerp(_zoomInitialPosition, _zoomCenter, percent);
+
+        transform.position = targetPosition;
+
+        _cameraStartMovePosition = targetPosition;
+    }
 }
--- ProjectPuzzle/Assets/Scripts/InputManager.cs
+++ ProjectPuzzle/Assets/Scripts/InputManager.cs
@@ -18,6 +18,11 @@ public class InputManager : MonoBehaviour
     [SerializeField] private PuzzleController _puzzleController;
     [SerializeField] private CameraController _cameraController;
 
+    [Header("Double touch")]
+    private Vector2 _firstTouchPosition;
+    private Vector2 _secondTouchPosition;
+    private Vector2 _initialDelta;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +41,57 @@ public class InputManager : MonoBehaviour
        {
             ManageSingleInput();
        }
+       else if(Input.touchCount == 2)
+       {
+            ManageDoubleInput();
+       }
+    }
+
+    private void ManageDoubleInput()
+    {
+        switch (_state)
+        {
+            case State.None:
+            case State.Camera:
+                ManageCameraDoubleTouchInput();
+                break;
+            case State.Puzzlepiece:
+                break;
+
+        }
+    }
+
+    private void ManageCameraDoubleTouchInput()
+    {
+        Touch[] touches = Input.touches;
+
+        if (touches[0].phase == TouchPhase.Began)
+        {
+            _firstTouchPosition = touches[0].position;
+        }
+
+        if (touches[1].phase == TouchPhase.Began)
+        {
+            _firstTouchPosition = touches[0].position;
+            _secondTouchPosition = touches[1].position;
+
+            _initialDelta = _firstTouchPosition - _secondTouchPosition;
+
+            _cameraController.DoubleTouchBeganCallback(_firstTouchPosition, _secondTouchPosition);
+        }
+
+        Vector2 currentDelta = touches[1].position - touches[0].position;
+        float deltaMagnitude = (currentDelta.magnitude - _initialDelta.magnitude) / Screen.width;
+
+        _cameraController.DoubleTouchDrag(deltaMagnitude);
+
+        foreach (Touch touch in touches)
+        {
+            if (touch.phase == TouchPhase.Ended)
+            {
+                _state = State.None;
+            }
+        }
     }
 
     private void ManageSingleInput()
